200e3caed6c03671fe39e838dc1ccca94239a507
Breno <brenosgt@github.com>
2021-12-20 22:38:02 -0300

    Login e Cadastro Usuario

---
+++ MyCritics/Data/MyCriticsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyCritics.Models;
+
+namespace MyCritics.Data
+{
+    public class MyCriticsContext : DbContext
+    {
+        public MyCriticsContext (DbContextOptions<MyCriticsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyCritics.Models.Usuario> Usuario { get; set; }
+    }
+}
---
+++ MyCritics/Models/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCritics.Models {
+    public class Usuario {
+        public int ID { get; set; }
+        public string Password { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public Usuario() {
+        }
+
+        public Usuario(int iD, string password, string nome, string sobrenome, string email, string cidade, string estado, DateTime dataNascimento) {
+            ID = iD;
+            Password = password;
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Email = email;
+            Cidade = cidade;
+            Estado = estado;
+            DataNascimento = dataNascimento;
+        }
+    }
+}
