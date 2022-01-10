9bbd7f178e3e6b6bfc2e4f1db2a206c2e4637946
Pacelle Henrique <pacellehenrique18@gmail.com>
2022-01-10 19:43:59 -0300

    Ajustes de front nas telas de login/layout/create(cadastro) e seus CSS. Também adicionado ao banco de dados novos usuários.

--- MyCritics/Migrations/20220108175819_pacellebd.Designer.cs
+++ MyCritics/Migrations/20220108175819_pacellebd.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyCritics.Data;
 
 namespace MyCritics.Migrations
 {
     [DbContext(typeof(MyCriticsContext))]
-    partial class MyCriticsContextModelSnapshot : ModelSnapshot
+    [Migration("20220108175819_pacellebd")]
+    partial class pacellebd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
